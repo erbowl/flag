36100f3205a11151726c66ab34c50affa6ac6f3c
瀬戸啓一朗 <shostakovich24@gmail.com>
2018-01-27 00:24:02 +0900

    目的地入力に対するapiの応答の実装

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+            <p>FLAG</p>
+            <DesForm/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -16,6 +16,67 @@ class App extends Component {
       </div>
     );
   }
+}
+const API_KEY='AIzaSyDhOCyIoQjMiCNcKdinj8cRHX32jMEDBOA';
+const REQUEST_URL='https://maps.googleapis.com/maps/api/geocode/json?address=ADDRESS&key='+API_KEY;
+class DesForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: '',data:'',address:''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        if(this.state.value!=''){
+            this.fetchData();
+            console.log(this.state.data);
+        }
+    }
+
+    fetchData() {
+     fetch(REQUEST_URL.replace("ADDRESS",this.state.value))
+          .then((response) => response.json())
+     .then((responseData) => {
+       this.setState({
+         data: responseData,
+       })
+       if(responseData.status=="OK"){
+           this.setState({
+               address:responseData.results[0].formatted_address
+           })
+       }
+     })
+    }
+
+    render() {
+        return (
+         <form onSubmit={this.handleSubmit}>
+             <label>
+              目的地（Destination）:
+                 <input type="text" value={this.state.value} onChange={this.handleChange} />
+             </label>
+             <p>{this.state.address}</p>
+         </form>
+         );
+    }
+}
+
+class distance extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={value:''};
+        this.handleChange=this.handleChange.bind(this);
+    }
+    handleChange(event){
+        this.setState({value:event.target.value});
+    }
+    render(){
+        return(
+            <p>{this.state.value}</p>
+        );
+    }
 }
 
 export default App;
