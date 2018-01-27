b4967a4147e0a9a2a587bb9d13074de006456bf0
瀬戸啓一朗 <shostakovich24@gmail.com>
2018-01-27 22:20:07 +0900

    方角を取得

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ class App extends Component {
         <header>
             <p>FLAG</p>
             <DesForm/>
-            <Location/>
-            <Distance />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -57,14 +55,20 @@ class DesForm extends React.Component {
 
     render() {
         return (
-         <form onSubmit={this.handleSubmit}>
-             <label>
-              目的地（Destination）:
-                 <input type="text" value={this.state.value} onChange={this.handleChange} />
-             </label>
-             <p>{this.state.address}</p>
-             <p>{this.state.lat},{this.state.lon}</p>
-         </form>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                 目的地（Destination）:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <p>{this.state.address}</p>
+                <p>目的地：{this.state.lat},{this.state.lon}</p>
+            </form>
+              <Location
+                des_lat={this.state.lat}
+                des_lon={this.state.lon}
+              />
+          </div>
          );
     }
 }
@@ -102,6 +106,12 @@ class Location extends React.Component{
         <div>
           <p>現在地：{this.state.lat},{this.state.lon}</p>
           <button onClick={this.getLocation}>現在地を取得</button>
+          <Distance
+            des_lat={this.props.des_lat}
+            des_lon={this.props.des_lon}
+            loc_lat={this.state.lat}
+            loc_lon={this.state.lon}
+          />
         </div>
       );
   }
@@ -110,22 +120,59 @@ class Location extends React.Component{
 class Distance extends React.Component{
     constructor(props){
         super(props);
+        this.geoDirection = this.geoDirection.bind(this);
         this.state={length:0,direction:0};
     }
+    componentWillReceiveProps(nextProps){
+      this.setState({length:this.geoDirection(nextProps.loc_lat,nextProps.loc_lon,nextProps.des_lat,nextProps.des_lon)});
+    }
 
-    // geoDirection(lat1, lng1, lat2, lng2) {
-    //   var Y = Math.cos(lng2 * Math.PI / 180) * Math.sin(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
-    //   var X = Math.cos(lng1 * Math.PI / 180) * Math.sin(lng2 * Math.PI / 180) - Math.sin(lng1 * Math.PI / 180) * Math.cos(lng2 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
-    //   var dirE0 = 180 * Math.atan2(Y, X) / Math.PI; // 東向きが０度の方向
-    //   if (dirE0 < 0) {
-    //     dirE0 = dirE0 + 360; //0～360 にする。
+    geoDirection(lat1, lng1, lat2, lng2) {
+      var Y = Math.cos(lng2 * Math.PI / 180) * Math.sin(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+      var X = Math.cos(lng1 * Math.PI / 180) * Math.sin(lng2 * Math.PI / 180) - Math.sin(lng1 * Math.PI / 180) * Math.cos(lng2 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+      var dirE0 = 180 * Math.atan2(Y, X) / Math.PI; // 東向きが０度の方向
+      if (dirE0 < 0) {
+        dirE0 = dirE0 + 360; //0～360 にする。
+      }
+      var dirN0 = (dirE0 + 90) % 360; //(dirE0+90)÷360の余りを出力 北向きが０度の方向
+      return dirN0;
+    }
+
+    // function geoDistance(lat1, lng1, lat2, lng2, precision) {
+    //   // 引数　precision は小数点以下の桁数（距離の精度）
+    //   var distance = 0;
+    //   if ((Math.abs(lat1 - lat2) < 0.00001) && (Math.abs(lng1 - lng2) < 0.00001)) {
+    //     distance = 0;
+    //   } else {
+    //     lat1 = lat1 * Math.PI / 180;
+    //     lng1 = lng1 * Math.PI / 180;
+    //     lat2 = lat2 * Math.PI / 180;
+    //     lng2 = lng2 * Math.PI / 180;
+    //
+    //     var A = 6378140;
+    //     var B = 6356755;
+    //     var F = (A - B) / A;
+    //
+    //     var P1 = Math.atan((B / A) * Math.tan(lat1));
+    //     var P2 = Math.atan((B / A) * Math.tan(lat2));
+    //
+    //     var X = Math.acos(Math.sin(P1) * Math.sin(P2) + Math.cos(P1) * Math.cos(P2) * Math.cos(lng1 - lng2));
+    //     var L = (F / 8) * ((Math.sin(X) - X) * Math.pow((Math.sin(P1) + Math.sin(P2)), 2) / Math.pow(Math.cos(X / 2), 2) - (Math.sin(X) - X) * Math.pow(Math.sin(P1) - Math.sin(P2), 2) / Math.pow(Math.sin(X), 2));
+    //
+    //     distance = A * (X + L);
+    //     var decimal_no = Math.pow(10, precision);
+    //     distance = Math.round(decimal_no * distance / 1) / decimal_no;   // kmに変換するときは(1000で割る)
     //   }
-    //   var dirN0 = (dirE0 + 90) % 360; //(dirE0+90)÷360の余りを出力 北向きが０度の方向
-    //   return dirN0;
+    //   return distance;
     // }
+
     render(){
         return(
-            <span>{this.state.value}</span>
+          <div>
+            <p>{this.state.length}</p>
+            <p>{this.props.loc_lat},{this.props.loc_lon},{this.props.des_lon},{this.props.des_lat}</p>
+          </div>
+
         );
     }
 }
